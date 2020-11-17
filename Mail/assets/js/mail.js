319d41b8d7b7c26a14107da4df6ee92b877674c1
angelacaterina <angela.caterina@icloud.com>
2020-11-17 19:14:59 +0100

    Mail: revisione finale

--- Mail/assets/js/mail.js
+++ Mail/assets/js/mail.js
@@ -12,10 +12,16 @@ var elEmail = userEmail;
 for(var i=0; i < emailList.length; i++){
   if(emailList[i] == elEmail){
     console.log("email vadila");
-    document.getElementById('benvenuto').innerHTML= "BENVENUTO " + userName;
+    document.getElementById('benvenuto').innerHTML= "BENVENUTO " + userName.toUpperCase();
+
+    document.getElementById('main_content').style.background = "radial-gradient(circle, rgba(115,246,146,1) 0%, rgba(47,131,6,1) 100%)";
   }else {
     console.log("email non valida");
-    document.getElementById('valisazione_email').innerHTML= "email non valida";
+    document.getElementById('valisazione_email').innerHTML= "! email non valida !";
+
+    document.getElementById('main_content').style.background = "radial-gradient(circle, rgba(214,65,65,1) 0%, rgba(110,0,0,1) 100%)";
   }
   break;
 }
+
+document.getElementById('card_rounded').style.backgroundColor = "white";
